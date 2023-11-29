14824d3427197f08a499224dee6d84fa699809de
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2023-11-29 13:03:08 +0530

    revised useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import classes from "./App.module.css";
 import Message from "./components/Message";
-import { Fragment } from "react";
+import { Fragment,useState } from "react";
 function App() {
   const items = ["coimbatore", "chennai", "mumbai"];
+  const [itemName,setItemName]=useState("chennai")
+  const handleClick=(e,item)=>{
+    e.preventDefault();
+    setItemName(item)
+  }
   if (items.length === 0) {
     return <p>Notjing to show</p>;
   }
@@ -17,7 +22,7 @@ function App() {
         <header>Learn React</header>
         <header>Learn React</header>
         {items.map((item) => (
-          <p key={item}>this is {item}</p>
+          <p key={item} className={item===itemName? classes.selected: classes.notActive} onClick={(e)=>handleClick(e,item)}>this is {item}</p>
         ))}
         <Message />
         {items.length !== 3 ? <p>There is more to learn</p> : <p>saranua</p>}
