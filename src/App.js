913d3e9d97268c62847aa08311b3d6c1940d0559
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2023-12-02 12:50:06 +0530

    learning new version react router

--- src/App.js
+++ src/App.js
@@ -1,54 +1,13 @@
-import classes from "./App.module.css";
-import Message from "./components/Message";
-import { Fragment, useState } from "react";
+
+import { Fragment } from "react";
+
 function App() {
-  // const items = ["coimbatore", "chennai", "mumbai"];
-  // const [itemName, setItemName] = useState("chennai");
-  // const handleClick = (item) => {
-  //   setItemName(item);
-  // };
-  // if (items.length === 0) {
-  //   return <p>Notjing to show</p>;
-  // }
-  const [alert, setAlert] = useState("Alert");
-  const handleClick = () => {
-    setAlert("Alert");
-  };
-  const handleClose = () => {
-    setAlert("");
-  };
+  
   return (
     <Fragment>
       <h1>hello world</h1>
-      <div className={classes.App}>
-        {alert.length >= 1 ? (
-          <>
-            <button onClick={handleClose}>X</button>
-            <header>Learn React</header>
-            <header>Learn React</header>
-            <header>Learn React</header>
-            <header>Learn React</header>
-            <header>Learn React</header>
-            <header>Learn React</header>
-            <p>Alert</p>
-            <p>Alert</p>
-            <p>Alert</p>
-          </>
-        ) : null}
+      
 
-        <Message clicked={() => handleClick()} />
-        {/* <Message
-          items={items}
-          itemName={itemName}
-          handleClick={(item) => handleClick(item)}
-        />
-        <Message
-          items={items}
-          itemName={itemName}
-          handleClick={(item) => handleClick(item)}
-        />
-        {items.length !== 3 ? <p>There is more to learn</p> : <p>saranua</p>} */}
-      </div>
     </Fragment>
   );
 }
